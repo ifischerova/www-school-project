d2c5e63e982ba69f9b8b513a8dbfa40172bd148c
Ivuska <fischerova.ivka@gmail.com>
2025-05-16 01:53:04 +0200

    Update postman, cleanup deps

--- springboot-app/src/main/java/cz/bezcisobe/controller/CertificationController.java
+++ springboot-app/src/main/java/cz/bezcisobe/controller/CertificationController.java
@@ -3,7 +3,6 @@ package cz.bezcisobe.controller;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import cz.bezcisobe.service.CertificationService;
--- springboot-app/src/main/java/cz/bezcisobe/model/Race.java
+++ springboot-app/src/main/java/cz/bezcisobe/model/Race.java
@@ -39,45 +39,5 @@ public class Race {
     private List<RideRequest> rideRequests;
 
     @OneToMany(mappedBy = "race")
-    private List<Ride> Ride;
-
-    public String getName() {
-      return name;
-    }
-
-    public String getPlace() {
-      return place;
-    }
-
-    public LocalDate getDate() {
-      return date;
-    }
-
-    public String getStartTime() {
-      return startTime;
-    }
-
-    public String getWeb() {
-      return web;
-    }
-
-    public void setName(String name) {
-      this.name = name;
-    }
-
-    public void setPlace (String place) {
-      this.place = place;
-    }
-
-    public void datumKonani(LocalDate date) {
-      this.date = date;
-    }
-
-    public void casKonani(String startTime) {
-      this.startTime = startTime;
-    }
-
-    public void setWeb(String web) {
-      this.web = web;
-    }
+    private List<Ride> rides;
 }
